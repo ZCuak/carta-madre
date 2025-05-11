05a6d17b4f38fd16167df4218c6937f0cea97b5f
ZCuak <gejomial@gmail.com>
2025-05-11 00:47:21 -0500

    Finalv2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Carta from './components/carta/Carta'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
+    <div className="min-h-screen">
       <Carta />
     </div>
   )
