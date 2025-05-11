3b7c93fcbf6b6266f957a93404c88ec6e06bb030
ZCuak <gejomial@gmail.com>
2025-05-11 01:07:20 -0500

    PruebaDeploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://zcuak.github.io/carta-madre/',
+  base: "/carta-madre",
 })
